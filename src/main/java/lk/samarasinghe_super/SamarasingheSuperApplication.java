2c212a5f034f74492442f05b45618738cd9ef0d8
LalithK90 <asakahatapitiya@gmail.com>
2021-03-17 12:39:28 +0530

    98% was completed

---
+++ src/main/java/lk/samarasinghe_super/SamarasingheSuperApplication.java
@@ -0,0 +1,13 @@
+package lk.samarasinghe_super;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamarasingheSuperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamarasingheSuperApplication.class, args);
+	}
+
+}
