2b0ce5537366600ca01886e8dd7785989deb5d1f
LalithK90 <asakahatapitiya@gmail.com>
2021-03-31 17:14:52 +0530

    addUser form was changed

--- src/main/java/lk/samarasingher_super/asset/employee/service/EmployeeFilesService.java
+++ src/main/java/lk/samarasingher_super/asset/employee/service/EmployeeFilesService.java
@@ -8,7 +8,6 @@ import lk.samarasingher_super.asset.employee.entity.Employee;
 import lk.samarasingher_super.asset.employee.entity.EmployeeFiles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
